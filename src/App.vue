18b61b1ad13e3e9228c15a9276bf9df091279f50
imac <imac@imacs-iMac.local>
2021-11-28 19:23:48 -0300

    Se sube desafio

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <ToDo></ToDo>
+  </div>
+</template>
+
+// Se importa componente
+<script>
+import ToDo from "./components/ToDo.vue";
+export default {
+  components: { ToDo },
+};
+</script>
+
+<style>
+</style>
